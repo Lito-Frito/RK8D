4ee89ffcf5bc5d4461df121ac8d3b7579e6441aa
crc8109 <crc8109@pm.me>
2022-06-05 17:59:02 -0400

    First commit

---
+++ Pong/README.md
@@ -0,0 +1,24 @@
+# RK8D - A retro, containerized gaming experience
+
+![image](https://user-images.githubusercontent.com/56422761/172072005-6aa99131-0e7b-4d78-a3af-68e92b561b4f.png)
+
+This is going to be a small project where I will recreate a few arcade games using Python. I'll then bundl the games and containerize them as a single image so people. This will ensure that anyone who can run Docker will be able to play my arcade games. I'll add more games in the future.
+
+
+## What This Includes
+`pong.py`: This is a python version of the game pong. It's a very bare-bones version of the game. I'll be adding new features to the game first and then containerizing it.
+
+
+# Getting Started
+
+## Requirements
+You'll need one of the following
+* Python 
+* Docker
+
+
+## Quick Start
+You can go to [Docker Hub](https://hub.docker.com/repository/docker/crc8109/RK8D) to see where I'm hosting the image for RK8D as it is now. It only includes the game pong for now.
+
+## Starting from Scratch
+Clone the repo. Then run `python3 pong.py`. That will start the only game made so far.
---
+++ Pong/pong.py
@@ -0,0 +1,135 @@
+from termios import TIOCPKT_DOSTOP
+import turtle
+
+wn = turtle.Screen()
+wn.title("Pong by @crc8109")
+wn.bgcolor("black")
+wn.setup(width=800, height=600)
+# tracer stops window from updating; speeds our game up
+wn.tracer(0)
+
+# Score
+score_a = 0
+score_b = 0
+
+# Paddle A
+paddle_a = turtle.Turtle()
+paddle_a.speed(0)
+paddle_a.shape("square")
+paddle_a.color("white")
+paddle_a.shapesize(stretch_wid=5, stretch_len=1)
+# Tells Turtle not to draw a line as it moves
+paddle_a.penup()
+paddle_a.goto(-350, 0)
+
+
+# Paddle B
+paddle_b = turtle.Turtle()
+paddle_b.speed(0)
+paddle_b.shape("square")
+paddle_b.color("white")
+paddle_b.shapesize(stretch_wid=5, stretch_len=1)
+paddle_b.penup()
+paddle_b.goto(350, 0)
+
+
+# Ball
+ball = turtle.Turtle()
+ball.speed(0)
+ball.shape("square")
+ball.color("white")
+ball.penup()
+ball.goto(0, 0)
+# speed factor
+ball.dx = .03
+ball.dy = .03
+
+
+# Pen
+pen = turtle.Turtle()
+pen.speed(0)
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 260)
+# Resets scorecard each time score is updated
+pen.clear()
+
+# TODO  Add ability for users to pass along their names
+pen.write("Player A: 0 Player B: 0", align="center", font=("Courier", 24, "normal"))
+
+
+# Control Functions
+def paddle_a_up():
+    y = paddle_a.ycor()
+    y += 15
+    paddle_a.sety(y)
+
+def paddle_a_down():
+    y = paddle_a.ycor()
+    y -= 15
+    paddle_a.sety(y)
+
+def paddle_b_up():
+    y = paddle_b.ycor()
+    y += 15
+    paddle_b.sety(y)
+
+def paddle_b_down():
+    y = paddle_b.ycor()
+    y -= 15
+    paddle_b.sety(y)
+
+
+# Keyboard Binding
+wn.listen()
+
+wn.onkeypress(paddle_a_up, "w")
+wn.onkeypress(paddle_a_up, "W")
+
+wn.onkeypress(paddle_a_down, "s")
+wn.onkeypress(paddle_a_down, "S")
+
+wn.onkeypress(paddle_b_up, "Up")
+wn.onkeypress(paddle_b_down, "Down")
+
+
+# Main game loop
+while True:
+    wn.update()
+
+    # Move ball
+    ball.setx(ball.xcor() + ball.dx)
+    ball.sety(ball.ycor() + ball.dy)
+
+    # Border boundary/physics
+    if ball.ycor() > 290:
+        # Reset ball to avoid any issues
+        ball.sety(290)
+        ball.dy *= -1
+
+    if ball.ycor() < -290:
+        ball.sety(-290)
+        ball.dy *= -1
+
+    if ball.xcor() > 390:
+        # Reset upon scoring
+        ball.goto(0, 0)
+        ball.dx *= -1
+        score_a += 1
+        pen.write("Player A: {} Player B: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
+    
+    if ball.xcor() < -390:
+        ball.goto(0, 0)
+        ball.dx *= -1
+        score_b += 1
+        pen.write("Player A: {} Player B: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
+
+    # Paddle and ball collisions
+    if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor() < paddle_b.ycor() + 50 and ball.ycor() > paddle_b.ycor() -50): # 50 ensures edges of paddle count as hits
+        ball.setx(340)
+        ball.dx *= -1
+    
+    if (ball.xcor() < -340 and ball.xcor() > -350) and (ball.ycor() < paddle_a.ycor() + 50 and ball.ycor() > paddle_a.ycor() -50): # 50 ensures edges of paddle count as hits
+        ball.setx(-340)
+        ball.dx *= -1
