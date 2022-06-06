91587d46b918f3d4558854d32951b34211bef271
crc8109 <crc8109@pm.me>
2022-06-06 17:07:58 -0400

    Remove error where importing Pong module runs game

--- Games/Pong/pong.py
+++ Games/Pong/pong.py
@@ -3,142 +3,148 @@ import turtle
 import random
 
 
-wn = turtle.Screen()
-wn.title("Pong by @crc8109")
-wn.bgcolor("black")
-wn.setup(width=800, height=600)
-# tracer stops window from updating; speeds our game up
-wn.tracer(0)
+def game_loop():
 
-# Score
-score_a = 0
-score_b = 0
+    wn = turtle.Screen()
+    wn.title("Pong by @crc8109")
+    wn.bgcolor("black")
+    wn.setup(width=800, height=600)
+    # tracer stops window from updating; speeds our game up
+    wn.tracer(0)
 
-# Paddle A
-# TODO add bumper physics to dx where SPACEBAR adds horizontal momentum
-paddle_a = turtle.Turtle()
-paddle_a.speed(0)
-paddle_a.shape("square")
-paddle_a.color("white")
-paddle_a.shapesize(stretch_wid=5, stretch_len=1)
-# Tells Turtle not to draw a line as it moves
-paddle_a.penup()
-paddle_a.goto(-350, 0)
+    # Score
+    score_a = 0
+    score_b = 0
 
-
-# Paddle B
-paddle_b = turtle.Turtle()
-paddle_b.speed(0)
-paddle_b.shape("square")
-paddle_b.color("white")
-paddle_b.shapesize(stretch_wid=5, stretch_len=1)
-paddle_b.penup()
-paddle_b.goto(350, 0)
+    # Paddle A
+    # TODO add bumper physics to dx where SPACEBAR adds horizontal momentum
+    paddle_a = turtle.Turtle()
+    paddle_a.speed(0)
+    paddle_a.shape("square")
+    paddle_a.color("white")
+    paddle_a.shapesize(stretch_wid=5, stretch_len=1)
+    # Tells Turtle not to draw a line as it moves
+    paddle_a.penup()
+    paddle_a.goto(-350, 0)
 
 
-# Ball
-ball = turtle.Turtle()
-ball.speed(0)
-ball.shape("square")
-ball.color("white")
-ball.penup()
-ball.goto(0, 0)
-# speed factor: random value between a certain rangeW
-ball.dx = random.uniform(0.02, 0.04)
-ball.dy = random.uniform(0.02, 0.04)
+    # Paddle B
+    paddle_b = turtle.Turtle()
+    paddle_b.speed(0)
+    paddle_b.shape("square")
+    paddle_b.color("white")
+    paddle_b.shapesize(stretch_wid=5, stretch_len=1)
+    paddle_b.penup()
+    paddle_b.goto(350, 0)
 
 
-# Pen/Scorecard
-pen = turtle.Turtle()
-pen.speed(0)
-pen.color("white")
-pen.penup()
-pen.hideturtle()
-pen.goto(0, 260)
-# TODO  Add ability for users to pass along their names
-pen.write("Player A: 0 Player B: 0", align="center", font=("Courier", 24, "normal"))
+    # Ball
+    ball = turtle.Turtle()
+    ball.speed(0)
+    ball.shape("square")
+    ball.color("white")
+    ball.penup()
+    ball.goto(0, 0)
+    # speed factor: random value between a certain range
+    ball.dx = random.uniform(0.02, 0.04)
+    ball.dy = random.uniform(0.02, 0.04)
 
 
-# Control Functions
-def paddle_a_up():
-    y = paddle_a.ycor()
-    y += 15
-    paddle_a.sety(y)
-
-def paddle_a_down():
-    y = paddle_a.ycor()
-    y -= 15
-    paddle_a.sety(y)
-
-def paddle_b_up():
-    y = paddle_b.ycor()
-    y += 15
-    paddle_b.sety(y)
-
-def paddle_b_down():
-    y = paddle_b.ycor()
-    y -= 15
-    paddle_b.sety(y)
+    # Pen/Scorecard
+    pen = turtle.Turtle()
+    pen.speed(0)
+    pen.color("white")
+    pen.penup()
+    pen.hideturtle()
+    pen.goto(0, 260)
+    # TODO  Add ability for users to pass along their names
+    pen.write("Player A: 0 Player B: 0", align="center", font=("Courier", 24, "normal"))
 
 
-# Keyboard Binding
-wn.listen()
+    # Control Functions
+    def paddle_a_up():
+        y = paddle_a.ycor()
+        y += 15
+        paddle_a.sety(y)
 
-wn.onkeypress(paddle_a_up, "w")
-wn.onkeypress(paddle_a_up, "W")
+    def paddle_a_down():
+        y = paddle_a.ycor()
+        y -= 15
+        paddle_a.sety(y)
 
-wn.onkeypress(paddle_a_down, "s")
-wn.onkeypress(paddle_a_down, "S")
+    def paddle_b_up():
+        y = paddle_b.ycor()
+        y += 15
+        paddle_b.sety(y)
 
-wn.onkeypress(paddle_b_up, "Up")
-wn.onkeypress(paddle_b_down, "Down")
+    def paddle_b_down():
+        y = paddle_b.ycor()
+        y -= 15
+        paddle_b.sety(y)
 
 
-# Main game loop
-# TODO add score limit or limit to # of rounds
-while True:
-    wn.update()
+    # Keyboard Binding
+    wn.listen()
 
-    # Move ball
-    ball.setx(ball.xcor() + ball.dx)
-    ball.sety(ball.ycor() + ball.dy)
+    wn.onkeypress(paddle_a_up, "w")
+    wn.onkeypress(paddle_a_up, "W")
 
-    # Border boundary/physics
+    wn.onkeypress(paddle_a_down, "s")
+    wn.onkeypress(paddle_a_down, "S")
 
-    # Top/Bottom boundaries
-    if ball.ycor() > 290:
-        # Reset ball to avoid any issues
-        ball.sety(290)
-        ball.dy *= -1
+    wn.onkeypress(paddle_b_up, "Up")
+    wn.onkeypress(paddle_b_down, "Down")
 
-    if ball.ycor() < -290:
-        ball.sety(-290)
-        ball.dy *= -1
 
-    # Left/Right boundaries
-    if ball.xcor() > 350:
-        # Reset upon scoring
-        ball.goto(0, 0)
-        ball.dy = -random.uniform(0.02, 0.04)
-        ball.dx = -random.uniform(0.02, 0.04)
-        score_a += 1
-        # Resets scorecard each time score is updated
-        pen.clear()
-        pen.write("Player A: {} Player B: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
-    
-    if ball.xcor() < -350:
-        ball.goto(0, 0)
-        ball.dy = random.uniform(0.02, 0.04)
-        ball.dx = random.uniform(0.02, 0.04)
-        score_b += 1
-        pen.clear()
-        pen.write("Player A: {} Player B: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
+    # Main game loop
+    # TODO add score limit or limit to # of rounds
 
-    # Paddle and ball collisions
-    if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor() < paddle_b.ycor() + 50 and ball.ycor() > paddle_b.ycor() -50): # 50 ensures edges of paddle count as hits
-        ball.setx(340)
-        ball.dx *= -1.1
-    
-    if (ball.xcor() < -340 and ball.xcor() > -350) and (ball.ycor() < paddle_a.ycor() + 50 and ball.ycor() > paddle_a.ycor() -50):
-        ball.setx(-340)
-        ball.dx *= -1.1
+    while True:
+        wn.update()
+
+        # Move ball
+        ball.setx(ball.xcor() + ball.dx)
+        ball.sety(ball.ycor() + ball.dy)
+
+        # Border boundary/physics
+
+        # Top/Bottom boundaries
+        if ball.ycor() > 290:
+            # Reset ball to avoid any issues
+            ball.sety(290)
+            ball.dy *= -1
+
+        if ball.ycor() < -290:
+            ball.sety(-290)
+            ball.dy *= -1
+
+        # Left/Right boundaries
+        if ball.xcor() > 350:
+            # Reset upon scoring
+            ball.goto(0, 0)
+            ball.dy = -random.uniform(0.02, 0.04)
+            ball.dx = -random.uniform(0.02, 0.04)
+            score_a += 1
+            # Resets scorecard each time score is updated
+            pen.clear()
+            pen.write("Player A: {} Player B: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
+        
+        if ball.xcor() < -350:
+            ball.goto(0, 0)
+            ball.dy = random.uniform(0.02, 0.04)
+            ball.dx = random.uniform(0.02, 0.04)
+            score_b += 1
+            pen.clear()
+            pen.write("Player A: {} Player B: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
+
+        # Paddle and ball collisions
+        if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor() < paddle_b.ycor() + 50 and ball.ycor() > paddle_b.ycor() -50): # 50 ensures edges of paddle count as hits
+            ball.setx(340)
+            ball.dx *= -1.1
+        
+        if (ball.xcor() < -340 and ball.xcor() > -350) and (ball.ycor() < paddle_a.ycor() + 50 and ball.ycor() > paddle_a.ycor() -50):
+            ball.setx(-340)
+            ball.dx *= -1.1
+
+if __name__ == "__game_loop__":
+    game_loop()
--- Games/main.py
+++ Games/main.py
@@ -1,9 +1,15 @@
-print("Hello friend.")
+import Pong.pong as pong
+
+print("Hello friends.")
 input("Let's play a few games!")
+
+player1 = input("\nPlayer1, what's your name? > ")
+print(f"Nice to meet you {player1}!")
+
+player2 = input("\nPlayer2, what's your name? > ")
+print("Nice to meet you too!")
 
 print("\nI'll boot up pong for you and a friend.")
 input("Booting... Press ENTER to commence!")
 
-import Pong.pong as game
-
-# game
+pong.game_loop()
