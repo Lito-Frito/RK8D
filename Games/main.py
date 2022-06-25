2c0f72befddaa3a3ce21a8bc34ee06c6c0bb86a9
crc8109 <crc8109@pm.me>
2022-06-25 00:24:54 -0400

    Add instructions for pong & give variable names for input lines

--- Games/main.py
+++ Games/main.py
@@ -2,7 +2,7 @@
 import Pong.pong as pong
 
 print("Hello friends.")
-input("Let's play a few games!")
+welcome = input("Let's play a few games!")
 
 player_a_too_long = True
 
@@ -10,10 +10,10 @@ while player_a_too_long:
 
     playerA = input("\nPlayerA, what's your name? > ")
     if len(playerA) > 12:
-        print("Unfortunately, that's too long to display...can you choose another name?")
+        print("Unfortunately, that's too long for me to display...can you choose another name?")
     
     else:
-        print(f"Nice to meet you {playerA}!")
+        greeting_a = input(f"Nice to meet you {playerA}!")
         player_a_too_long = False
 
 
@@ -22,14 +22,19 @@ player_b_too_long = True
 while player_b_too_long:
     playerB = input("\nPlayerB, what's your name? > ")
     if len(playerB) > 12:
-        print("Unfortunately, that's too long to display...can you choose another name?")
+        print("Unfortunately, that's too long for me to display...can you choose another name?")
 
     else:
-        print(f"Nice to meet you {playerA}!")
+        greeting_b = input(f"Nice to meet you {playerB}!")
         player_b_too_long = False
     
 
-print("\nI'll boot up pong for you and a friend.")
-input("Booting... Press ENTER to commence!")
+boot = input("\nI'll boot up pong for you both.")
+
+instructions = input(f"\n{playerA}, you use W & S to move your paddle."
+                     f"\n{playerB}, use the UP and DOWN arrows to move your paddle."
+                      "\nFirst to 5 points wins!")
+
+start_game = input("\nBooting... Press ENTER to commence!")
 
 pong.game_loop(playerA, playerB)
